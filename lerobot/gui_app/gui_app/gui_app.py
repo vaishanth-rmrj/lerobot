74283c3e73f2ce1510dccd7500069ec7fdc4dcad
vaishanth_r <vaishanth.rmrj@gmail.com>
2024-12-04 21:08:23 +0000

    gui minor changes

--- lerobot/gui_app/gui_app/gui_app.py
+++ lerobot/gui_app/gui_app/gui_app.py
@@ -8,30 +8,18 @@ from rxconfig import config
 from .state import State
 
 
-
+# @rx.page(on_load=State.on_cont_mount)
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
-        rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+            rx.image(src=State.image),
+            rx.text(State.image_text),
+            rx.button("Teleoperate", on_click=State.teleop),
         ),
-        rx.logo(),
+        
+        # on_unmount=State.on_cont_unmount,
     )
-
 
 app = rx.App()
 app.add_page(index)
--- lerobot/gui_app/gui_app/state.py
+++ lerobot/gui_app/gui_app/state.py
@@ -1,6 +1,99 @@
 import reflex as rx
 
-class State(rx.State):
-    """The app state."""
+import asyncio
+from PIL import Image
+import requests
+import cv2
 
-    ...
+class State(rx.State):
+    url = f"https://picsum.photos/id/1/200/300"
+    image = Image.open(requests.get(url, stream=True).raw)
+    image_text:str = "hello"
+
+    video_captures = {}
+
+    is_cam_feed_fetch_active: bool = False
+
+    def get_video_capture(self, device_id: int):
+        if device_id not in self.video_captures:
+            cap = cv2.VideoCapture(device_id)
+            cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+            self.video_captures[device_id] = cap
+        return self.video_captures[device_id]
+
+
+# def generate_frames(device_id: int):
+    
+#     while True:
+#         success, frame = cap.read()
+#         if not success:
+#             break
+#         else:
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             if not ret:
+#                 continue
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+    @rx.event(background=True)
+    async def fetch_cam_feed(self):
+
+        # async with self:    
+        #     if self.is_cam_feed_fetch_active:
+        #         print("Background event already running")
+        #         return 
+
+        async with self:
+            cap = self.get_video_capture(0)
+
+        counter = 0
+        while True:
+            print("looping")
+
+            async with self:
+                success, frame = cap.read()
+                self.image = Image.fromarray(frame)
+            if not success:
+                break
+            # else:
+            #     async with self:
+            #         ret, self.image = cv2.imencode('.jpg', frame)
+
+            # async with self:
+            #     self.image_text = f"hello {counter}"
+            
+            #     if not self.is_cam_feed_fetch_active:
+            #         return 
+            
+            # counter += 1
+
+            await asyncio.sleep(0.001)
+
+
+    @rx.event
+    def on_cont_mount(self):
+        print("mount function called")
+        if not self.is_cam_feed_fetch_active:
+            self.is_cam_feed_fetch_active = True
+            return State.fetch_cam_feed
+
+    @rx.event
+    def on_cont_unmount(self):
+        print("unmount function called")
+        if self.is_cam_feed_fetch_active:
+            self.is_cam_feed_fetch_active = False
+
+            
+
+    def enable_cam_feed(self):
+        self.is_cam_feed_fetch_active = True
+        return State.fetch_cam_feed
+        
+    def teleop(self):
+        print("Clicked on teleop")
+        if not self.is_cam_feed_fetch_active:
+            self.is_cam_feed_fetch_active = True
+            return State.fetch_cam_feed
+
+    
