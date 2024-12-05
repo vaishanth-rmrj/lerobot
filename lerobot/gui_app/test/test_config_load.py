44e4e53e131fcacc1ff9bf6637dcfa242810b94c
vaishanth_r <vaishanth.rmrj@gmail.com>
2024-12-05 16:46:36 +0000

    minor updates

--- lerobot/gui_app/test/test_config_load.py
+++ lerobot/gui_app/test/test_config_load.py
@@ -4,7 +4,9 @@ CFG_FILE_PATH = "lerobot/gui_app/configs/mode_cfg.yaml"
 
 app_cfg = init_hydra_config(CFG_FILE_PATH)
 
+print(type(app_cfg))
+
 print(app_cfg.robot_cfg_file)
-print(app_cfg.fps)
 
 print(app_cfg.record.root)
+print(app_cfg.record.fps)
