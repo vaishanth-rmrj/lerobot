b7580ae0bb5d6e4355d6726f78bc732bb56666a4
vaishanth_r <vaishanth.rmrj@gmail.com>
2025-02-18 17:30:02 +0000

    update minor

--- lerobot/common/policies/nomad/configuration_nomad.py
+++ lerobot/common/policies/nomad/configuration_nomad.py
@@ -3,13 +3,17 @@ from dataclasses import dataclass, field
 
 from lerobot.configs.policies import PreTrainedConfig
 
-@PreTrainedConfig.register_subclass("diffusion")
+@PreTrainedConfig.register_subclass("nomad")
 @dataclass
-class DiffusionConfig(PreTrainedConfig):
+class NomadConfig(PreTrainedConfig):
     """Configuration class for NomadPolicy.
     """
 
     # Inputs / output structure.
-    n_obs_steps: int = 2
-    horizon: int = 16
+    n_obs_steps: int = 4
+    horizon: int = 8
     n_action_steps: int = 8
+
+    # Architecture / modeling.
+    # Vision backbone.
+    vision_backbone: str = "efficientnet-b0"
--- lerobot/common/policies/nomad/modeling_nomad.py
+++ lerobot/common/policies/nomad/modeling_nomad.py
@@ -1,14 +1,88 @@
 import torch
 import torch.nn as nn
+from transformers import EfficientNetModel
+
+from lerobot.common.policies.nomad.configuration_nomad import NomadConfig
 
 class NomadPolicy(nn.Module):
     def __init__(self):
         super().__init__()
 
 class NomadVisionEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, config: NomadConfig):
+        """
+        Vision encoder model uses CNN(efficientnet-b0) and transformers to convert
+        images to tokens
+
+        Args:
+            config (NomadConfig): _description_
+        """
         super().__init__()
+        self.config = config
+
+        # cnn encode
+        if config.vision_backbone.startswith("efficientnet"):
+            self.cnn_encoder = EfficientNetModel.from_pretrained("google/"+config.vision_backbone)
+        else:
+            raise NotImplementedError
 
 class DiffusionConditionalUnet1d(nn.Module):
     def __init__(self):
         super().__init__()
+
+
+# Utils for Group Norm
+def replace_bn_with_gn(
+    root_module: nn.Module,
+    features_per_group: int=16) -> nn.Module:
+    """
+    Relace all BatchNorm layers with GroupNorm.
+    """
+    replace_submodules(
+        root_module=root_module,
+        predicate=lambda x: isinstance(x, nn.BatchNorm2d),
+        func=lambda x: nn.GroupNorm(
+            num_groups=x.num_features//features_per_group,
+            num_channels=x.num_features)
+    )
+    return root_module
+
+
+def replace_submodules(
+        root_module: nn.Module,
+        predicate: Callable[[nn.Module], bool],
+        func: Callable[[nn.Module], nn.Module]) -> nn.Module:
+    """
+    Replace all submodules selected by the predicate with
+    the output of func.
+
+    predicate: Return true if the module is to be replaced.
+    func: Return new module to use.
+    """
+    if predicate(root_module):
+        return func(root_module)
+
+    bn_list = [k.split('.') for k, m
+        in root_module.named_modules(remove_duplicate=True)
+        if predicate(m)]
+    for *parent, k in bn_list:
+        parent_module = root_module
+        if len(parent) > 0:
+            parent_module = root_module.get_submodule('.'.join(parent))
+        if isinstance(parent_module, nn.Sequential):
+            src_module = parent_module[int(k)]
+        else:
+            src_module = getattr(parent_module, k)
+        tgt_module = func(src_module)
+        if isinstance(parent_module, nn.Sequential):
+            parent_module[int(k)] = tgt_module
+        else:
+            setattr(parent_module, k, tgt_module)
+    # verify that all modules are replaced
+    bn_list = [k.split('.') for k, m
+        in root_module.named_modules(remove_duplicate=True)
+        if predicate(m)]
+    assert len(bn_list) == 0
+    return root_module
+
+
